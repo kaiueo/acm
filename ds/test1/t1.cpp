123824af7274db5f6ffed6f0c1691a571ccbe869
kaiueo <kaiueo@outlook.com>
2015-03-18 13:09:28 +0800

    test1 AC

--- ds/test1/t1.cpp
+++ ds/test1/t1.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
 #include <cstdio>
+#include <cmath>
 using namespace std;
 int main(){
 
+    int k;
+    while(scanf("%d",&k)!=EOF){
+        for(int i = 1;i<=k;i++){
+            for(int j = 1;j<=k;j++){
+                int a = min(abs(i),min(abs(k-i+1),min(abs(j),abs(k-j+1))));
+                if(a%2==0){
+                    printf("#");
+                }
+                else{
+                    printf("*");
+                }
+            }
+            printf("\n");
+        }
+        printf("\n");
+    }
     return 0;
 }
