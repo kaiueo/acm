c6a6f776d29e446af6b5ebff1f6f439b005bc754
kaiueo <kaiueo@outlook.com>
2015-05-28 21:17:10 +0800

    Add some functions of BinTree

--- ds/ds/tree/bintree.cpp
+++ ds/ds/tree/bintree.cpp
@@ -11,6 +11,64 @@ typedef struct node{
     struct node* rchild;
 }BiTNode, *BinTree;
 
+typedef BinTree Datatype;
+typedef struct snode{
+    Datatype data;
+    snode* next;
+}LinkNode, *LinkStack;
+
+void initStack(LinkStack &S){
+    S = (LinkStack)malloc(sizeof(LinkNode));
+    S->next = NULL;
+}
+
+//Pay attention to the judgement.
+int StackEmpty(LinkStack &S){
+    if(S->next){
+        return 0;
+    }
+    return 1;
+}
+
+//The e here, don't need the &.
+void push(LinkStack &S, Datatype e){
+    LinkStack p = (LinkStack)malloc(sizeof(LinkNode));
+    p->data = e;
+    p->next = S->next;
+    S->next = p;
+}
+
+int pop(LinkStack &S, Datatype &e){
+    if(StackEmpty(S)){
+        return 0;
+    }
+    LinkStack p = S->next;
+    e = p->data;
+    S->next = p->next;
+    free(p);
+    return 1;
+}
+
+int gettop(LinkStack &S, Datatype &e){
+    if(StackEmpty(S)){
+        return 0;
+    }
+    LinkStack p = S->next;
+    e = p->data;
+    return 1;
+}
+
+int StackSize(LinkStack &S){
+    LinkStack p = S->next;
+    int length = 0;
+    while(p){
+        p = p->next;
+        length++;
+    }
+    return length;
+}
+
+
 void InOrder(BinTree &T){
     if(T!=NULL){
         InOrder(T->lchild);
@@ -108,12 +166,74 @@ int Equal(BinTree &S, BinTree &T){
     return 1;
 }
 
+void PreOrderS(BinTree &BT){
+    LinkStack S;
+    initStack(S);
+    BinTree p = BT;
+    while(p!=NULL||!StackEmpty(S)){
+        while(p!=NULL){
+            cout<<p->data<<endl;
+            push(S, p);
+            p = p->lchild;
+        }
+        if(!StackEmpty(S)){
+            pop(S,p);
+            p = p->rchild;
+        }
+    }
+}
+
+void InOrderS(BinTree BT){
+    LinkStack S;
+    initStack(S);
+    BinTree p = BT;
+    while(p!=NULL||!StackEmpty(S)){
+        while(p!=NULL){
+            push(S, p);
+            p = p->lchild;
+        }
+        if(!StackEmpty(S)){
+            pop(S, p);
+            cout<<p->data<<endl;
+            p = p->rchild;
+        }
+    }
+}
+
+void PostOrderS(BinTree BT){
+    LinkStack S;
+    initStack(S);
+    BinTree cur;
+    BinTree pre = NULL;
+    if(BT){
+        push(S, BT);
+    }
+    while(!StackEmpty(S)){
+        gettop(S, cur);
+        if((cur->lchild==NULL&&cur->rchild==NULL)||(pre!=NULL&&(pre==cur->lchild||pre==cur->rchild))){
+            cout<<cur->data<<endl;
+            BinTree tmp;
+            pop(S, tmp);
+            pre = cur;
+        }
+        else{
+            if(cur->rchild!=NULL){
+                push(S, cur->rchild);
+            }
+            if(cur->lchild!=NULL){
+                push(S, cur->lchild);
+            }
+        }
+    }
+}
+
 int main(){
     BinTree T;
     createBinTree(T);
-    PreOrder(T);
-    InOrder(T);
+
     PostOrder(T);
+    cout<<endl;
+    PostOrderS(T);
     printBinTree(T);
     cout<<endl;
     cout<<Height(T)<<endl;
